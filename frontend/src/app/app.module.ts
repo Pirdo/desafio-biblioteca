ad366bfcc30b2490c5cee1ee8fe8c4a480ee735c
Pirdo <gabrielfpires0@gmail.com>
2022-07-16 14:07:48 -0300

    styling header and footer template

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,15 +14,26 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
+import { FooterComponent } from './components/template/footer/footer.component';
+import { LivrosCrudComponent } from './components/views/livros-crud/livros-crud.component';
+import { HomeComponent } from './components/views/home/home.component';
+
 @NgModule({
-    declarations: [AppComponent, HeaderComponent],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        LivrosCrudComponent,
+        HomeComponent
+    ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
         MatToolbarModule,
         MatButtonModule,
-        MatFormFieldModule
+        MatFormFieldModule,
+        MatCardModule
     ],
     providers: [],
     bootstrap: [AppComponent]
