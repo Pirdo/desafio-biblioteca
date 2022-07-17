c2f274f0ac6125cd9fe69b0b2aa6ccaa9ce056d7
Pirdo <gabrielfpires0@gmail.com>
2022-07-16 23:46:31 -0300

    adding routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+import { LivrosCrudComponent } from './components/views/livros-crud/livros-crud.component';
+import { HomeComponent } from './components/views/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'biblioteca',
+        component: LivrosCrudComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
