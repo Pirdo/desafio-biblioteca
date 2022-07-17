166c29d4f3b0909717145b0a73db5b626031f0f9
Pirdo <gabrielfpires0@gmail.com>
2022-07-16 23:43:38 -0300

    creating livros view component

---
+++ frontend/src/app/components/views/livros-crud/livros-crud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-livros-crud',
+  templateUrl: './livros-crud.component.html',
+  styleUrls: ['./livros-crud.component.css']
+})
+export class LivrosCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
