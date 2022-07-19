1c745656e3e684fdfc36a2f1c43058e959ebbf51
Pirdo <gabrielfpires0@gmail.com>
2022-07-18 22:35:15 -0300

    styling livro-update component

--- frontend/src/app/components/livros/livro-update/livro-update.component.ts
+++ frontend/src/app/components/livros/livro-update/livro-update.component.ts
@@ -1,15 +1,17 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-livro-update',
-  templateUrl: './livro-update.component.html',
-  styleUrls: ['./livro-update.component.css']
+    selector: 'app-livro-update',
+    templateUrl: './livro-update.component.html',
+    styleUrls: ['./livro-update.component.css']
 })
 export class LivroUpdateComponent implements OnInit {
+    constructor(private router: Router) {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+    cancel(): void {
+        this.router.navigate(['/biblioteca']);
+    }
 }
