8e6dfe13726c198080658264cccb932adf41d969
Pirdo <gabrielfpires0@gmail.com>
2022-08-10 10:34:07 -0300

    updating server.js file

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 
 const cors = require('cors');
@@ -6,8 +7,13 @@ const pool = require('./db/conn');
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json);
+app.use(cors());
+
+app.use('/health', (request, response) =>
+    response.json({ health: "application it's work" })
+);
 
 app.get('/livros/adicionarlivros', (req, res) => {
     const titulo = req.body.titulo;
@@ -15,9 +21,19 @@ app.get('/livros/adicionarlivros', (req, res) => {
     const editora = req.body.editora;
     const publicacao = req.body.publicacao;
 
-    const sql = `INSERT INTO livros (livro_titulo, livro_autor, livro_editora, livro_publicacao) VALUES ('${titulo}', '${autor}', '${editora}', '${publicacao}')`;
+    const sql = `INSERT INTO livros (??, ??, ??, ??) VALUES (?, ?, ?, ?)`;
+    const data = [
+        'livro_titulo',
+        'livro_autor',
+        'livro_editora',
+        'livro_publicacao',
+        titulo,
+        autor,
+        editora,
+        publicacao
+    ];
 
-    pool.query(sql, function (err) {
+    pool.query(sql, data, function (err) {
         if (err) {
             console.log(err);
             return;
@@ -29,9 +45,10 @@ app.get('/livros/adicionarlivros', (req, res) => {
 app.get('/livros/editarlivros/:id', (req, res) => {
     const id = req.params.id;
 
-    const sql = `SELECT * FROM livros WHERE id = ${id}`;
+    const sql = `SELECT * FROM livros WHERE ?? = ?`;
+    const data = ['id', id];
 
-    pool.query(sql, function (err) {
+    pool.query(sql, data, function (err, data) {
         if (err) {
             console.log(err);
             return;
@@ -49,9 +66,21 @@ app.post('/livros/atualizarlivros', (req, res) => {
     const editora = req.body.editora;
     const publicacao = req.body.publicacao;
 
-    const sql = `UPDATE livros SET livro_titulo = '${titulo}', livro_autor = '${autor}', livro_editora = '${editora}', livro_publicacao = '${publicacao}' WHERE id = ${id}`;
+    const sql = `UPDATE livros SET ?? = ?, ?? = ?, ?? = ?', ?? = ? WHERE ?? = ?`;
+    const data = [
+        'livro_titulo',
+        titulo,
+        'livro_autor',
+        autor,
+        'livro_editora',
+        editora,
+        'livro_publicacao',
+        publicacao,
+        'id',
+        id
+    ];
 
-    pool.query(sql, function (err) {
+    pool.query(sql, data, function (err, data) {
         if (err) {
             console.log(err);
             return;
@@ -65,9 +94,10 @@ app.post('/livros/atualizarlivros', (req, res) => {
 app.post('/livros/deletarlivros/:id', (req, res) => {
     const id = req.params.id;
 
-    const sql = `DELETE FROM livros WHERE id = ${id}`;
+    const sql = `DELETE FROM livros WHERE ?? = ?`;
+    const data = ['id', id];
 
-    pool.query(sql, function (err) {
+    pool.query(sql, data, function (err) {
         if (err) {
             console.log(err);
             return;
@@ -77,8 +107,6 @@ app.post('/livros/deletarlivros/:id', (req, res) => {
     });
 });
 
-app.get('/', (req, res) => {
-    res.render('frontend/src/app/app.component.html');
-});
+app.get('/', (req, res) => {});
 
-app.listen(process.env.PORT || 3000);
+app.listen(3000);
